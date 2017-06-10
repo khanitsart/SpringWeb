431ddc89a8d9c6d0e2a953ba356519746c9e83c0
khanitsart <khanitsartnet@gmail.com>
2017-06-10 23:55:34 +0700

    Add bootstrap and jquery dependencies to pom

---
+++ src/main/java/com/khanitsart/dev/SpringWebAwsApplication.java
@@ -0,0 +1,12 @@
+package com.khanitsart.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebAwsApplication.class, args);
+	}
+}
