7a61fb53721f8eb60e99fc6cc432a474270d4f04
khanitsart <khanitsartnet@gmail.com>
2017-06-12 01:33:00 +0700

    Add payload page.

---
+++ src/main/java/com/khanitsart/dev/web/controllers/PayloadController.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.khanitsart.dev.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author beatrixiii Khanitsart P.
+ *
+ */
+@Controller
+public class PayloadController {
+	
+	public static final String PAYLOAD_VIEW_NAME = "payload/payload";
+
+    @RequestMapping("/payload")
+    public String payload() {
+        return PAYLOAD_VIEW_NAME;
+    }
+}
